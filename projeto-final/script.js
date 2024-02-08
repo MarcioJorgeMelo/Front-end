6d2dcb5077e4ee3d4a4f4441b56e0c9fc6c3ac25
MarcioJorgeMelo <142255861+MarcioJorgeMelo@users.noreply.github.com>
2024-02-08 14:57:47 -0300

    -1

--- projeto-final/script.js
+++
@@ -1,6 +0,0 @@
-function mostrar(){
-    var a = window.document.getElementById("iframe")
-    var b = window.document.getElementById("container")
-    a.style.display = "block"
-    b.style.display = "block"
-}
